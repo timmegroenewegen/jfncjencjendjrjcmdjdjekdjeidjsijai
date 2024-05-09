346a686573c5a6d52626c749922995d363b948d0
timmegroenewegen <95966823+timmegroenewegen@users.noreply.github.com>
2024-05-09 11:40:00 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onPinPressed(TouchPin.P0, function () {
+    turtle.turnLeft()
+})
+input.onButtonPressed(Button.A, function () {
+    turtle.forward(1)
+})
+input.onButtonPressed(Button.B, function () {
+    turtle.back(1)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    turtle.turnRight()
+})
+basic.forever(function () {
+	
+})
